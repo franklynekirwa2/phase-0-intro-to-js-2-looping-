4593111056f4c7cc84620edd7e9338571af3fbf8
franklynekirwa2 <sangkirwa2@gmail.com>
2024-07-04 15:00:18 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(name, event){
+    const Messages = [];
+    for ( let i = 0; i < name.length; i++){
+        Messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return Messages;
+}
+function countDown(number) {
+    while(number >= 0){
+        console.log(number);
+        number--;
+    }
+}
+
+countDown();
+
